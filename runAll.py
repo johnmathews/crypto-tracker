21c509cc98c2d327e6bcbccca9759421c014f4d2
JohnMathews <mthwsjc@gmail.com>
2018-07-09 20:51:18 +0200

    first commit

---
+++ runAll.py
@@ -0,0 +1,44 @@
+
+# coding: utf-8
+
+# # Table of Contents
+#  <p>
+
+# In[1]:
+
+
+import os
+notebookDir = '/Users/john/projects/cryptoTracker2' 
+notebookDir2 = '/Users/john/projects/cryptoTracker' 
+
+
+# In[2]:
+
+
+os.chdir(notebookDir)
+get_ipython().magic("run ./'CryptoTracker p1.ipynb'")
+print("part 1 complete")
+os.chdir(notebookDir)
+
+
+# In[3]:
+
+
+get_ipython().magic("run ./'CryptoTracker p2.ipynb'")
+print("part 2 complete")
+os.chdir(notebookDir)
+
+
+# In[4]:
+
+
+get_ipython().magic("run ./'CryptoTracker p3.ipynb'")
+print("part 3 complete")
+os.chdir(notebookDir)
+
+
+# In[ ]:
+
+
+
+
